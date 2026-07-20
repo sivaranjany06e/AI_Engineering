2355af4437f9c8383bc070cd19a7367b23f648d7
sivaranjany06e <sivaranjany06e@gmail.com>
2026-07-21 00:40:03 +0530

    Start machine learning module

---
+++ 06_Machine_Learning/01_intro.py
@@ -0,0 +1,14 @@
+print("Welcome to Machine Learning!")
+
+applications = [
+    "Spam Detection",
+    "House Price Prediction",
+    "Fraud Detection",
+    "Recommendation System",
+    "Face Recognition"
+]
+
+print("\nApplications of Machine Learning:")
+
+for app in applications:
+    print("-", app)
