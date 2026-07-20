1d73b7f44ef6d27074308aa66f1554a8c0309afa
sivaranjany06e <sivaranjany06e@gmail.com>
2026-07-20 21:34:32 +0530

    Add python operators lesson

---
+++ 01_Python/operators.py
@@ -0,0 +1,55 @@
+#1️⃣ Arithmetic Operators
+#Used for calculations.
+
+a = 20
+b = 10
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a // b)  # Floor Division
+print(a % b)   # Modulus (Remainder)
+print(a ** b)  # Exponent
+
+# 2️⃣ Comparison Operators
+
+# These compare two values and return True or False.
+
+print(20 > 10)
+print(20 < 10)
+print(20 == 10)
+print(20 != 10)
+print(20 >= 10)
+print(20 <= 10)
+
+# 3️⃣ Logical Operators
+
+# Combine conditions.
+
+age = 25
+salary = 800000
+
+print(age > 18 and salary > 500000)
+print(age > 30 or salary > 500000)
+print(not(age > 18))
+
+#4️⃣ Assignment Operators
+
+x = 10
+
+x += 5
+print(x)
+
+x *= 2
+print(x)
+
+# 🎯 Interview Questions
+
+# Try to answer these after completing the code:
+
+# What is an operator?
+# What is the difference between / and //?
+# What is the difference between = and ==?
+# What does % return?
+# What is the difference between and and or?
