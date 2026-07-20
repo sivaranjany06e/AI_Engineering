7c3895db0417071286b0f47a5502737ad9581b0c
sivaranjany06e <sivaranjany06e@gmail.com>
2026-07-20 20:48:06 +0530

    Add Python variables lesson

---
+++ 01_Python/variables.py
@@ -0,0 +1,48 @@
+# ==========================================
+# Variables in Python
+# ==========================================
+
+# String
+name = "Sivaranjany"
+
+# Integer
+age = 29
+
+# Float
+height = 150.5
+
+# Boolean
+is_ai_engineer = False
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("AI Engineer:", is_ai_engineer)
+
+favorite_language = "Python"
+years_of_experience = 2.5
+salary = 1000000
+is_learning_ai = True
+
+print(favorite_language)
+print(years_of_experience)
+print(salary)
+print(is_learning_ai)
+
+
+# 💡 Interview Notes
+
+# Q1. What is a variable?
+
+# Answer:
+
+# A variable is a named storage location used to store data in memory. Its value can be accessed and modified during program execution.
+
+# Q2. Why do we use variables?
+
+# Answer:
+
+# Store data
+# Reuse values
+# Make code readable
+# Avoid repeating values
