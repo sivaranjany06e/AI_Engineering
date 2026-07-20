56c3353aba2983be4dbbb30e0c3730406c5006ef
sivaranjany06e <sivaranjany06e@gmail.com>
2026-07-20 21:26:06 +0530

    Add Python data types lesson

---
+++ 01_Python/data_types.py
@@ -0,0 +1,66 @@
+# ==========================================
+# Python Data Types
+# ==========================================
+
+# String
+name = "Sivaranjany"
+
+# Integer
+age = 29
+
+# Float
+salary = 10.5
+
+# Boolean
+is_learning_ai = True
+
+# List
+skills = ["Python", "Git", "NumPy"]
+
+# Tuple
+location = ("Tamil Nadu", "India")
+
+# Set
+numbers = {10, 20, 30}
+
+# Dictionary
+student = {
+    "name": "Sivaranjany",
+    "role": "AI Engineer",
+    "experience": 2.5
+}
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_learning_ai))
+print(type(skills))
+print(type(location))
+print(type(numbers))
+print(type(student))
+
+course = "AI Engineering"
+duration = 12
+fees = 0.0
+completed = False
+
+technologies = ["Python", "Git", "VS Code"]
+
+countries = ("India", "USA", "Canada")
+
+unique_numbers = {5, 10, 15, 20}
+
+employee = {
+    "id": 101,
+    "name": "Siva",
+    "department": "AI"
+}
+
+print(type(course))
+print(type(duration))
+print(type(fees))
+print(type(completed))
+print(type(technologies))
+print(type(countries))
+print(type(unique_numbers))
+print(type(employee))
